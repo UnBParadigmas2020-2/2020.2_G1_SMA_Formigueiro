90cf62d847cd7fa0ab0ed8a3f52deb36dc1cd914
Elias Bernardo <ebmm01@gmail.com>
2021-05-01 18:22:03 -0300

    Refactoring main
    Co-authored-by: Fernando Miranda <ilusdasilva@gmail.com>

--- main.py
+++ main.py
@@ -4,83 +4,30 @@ from random import choice
 from pade.acl.aid import AID
 from pade.behaviours.protocols import TimedBehaviour
 from src.agents.Canvas import CanvasAgent
+from src.agents.Ant import Ant
 from sys import argv
 import numpy as np
 import pygame
 
 
-class ComportTemporal(TimedBehaviour):
-    def __init__(self, agent, time):
-        super(ComportTemporal, self).__init__(agent, time)
-
-    def on_time(self):
-        super(ComportTemporal, self).on_time()
-        a = self.agent.position
-        a = [a-11, a-10, a-9, a-1, a+1, a+11, a+10, a+9]
-        possible_paths = []
-
-        for position in a:
-            if position >= 0 and position <= 1000:
-                possible_paths.append(position)
-
-        self.agent.position = self.agent.choose_path(possible_paths)
-        display_message(self.agent.aid.localname, self.agent.grid[self.agent.position])
-        
-
-class Ant(Agent):
-    position = 499
-
-    def __init__(self, aid, grid, food_position):
-        super(Ant, self).__init__(aid=aid)
-        self.grid = grid
-        self.food_position = food_position
-        self.carrying = False
-        comp_temp = ComportTemporal(self, .1)
-
-        self.behaviours.append(comp_temp)
-
-    def choose_path(self, possible_paths:list):
-        preferred_path = []
-        secondary_path = []
-
-        for position in possible_paths:
-            if self.grid[position][1] < 1:              #prefere andar por caminhos sem feromonio "home"
-                preferred_path.append(position)
-            else:
-                secondary_path.append(position)
-
-        if not self.carrying:
-            self.grid[self.position] += [0,1,0]
-            if len(preferred_path) > 0:
-                if self.food_position in preferred_path:
-                    self.carrying = True
-                    return self.food_position
-                else:
-                    return choice(preferred_path)
-            else:
-                return choice(secondary_path)
-        else:
-            return self.food_position
-
-
 if __name__ == '__main__':
 
-    grid = np.column_stack((np.arange(1000).reshape(1000,1),np.zeros((1000,2),dtype=int)))
-    agents_per_process = 10
-    food_position = 769
-    c = 0
+    agents_per_process = 1
     agents = []
-    agents.append(CanvasAgent(AID(name="Canvas_Agent")))
-    c += 1000
+
+    c = 0
+    #agents.append(CanvasAgent(AID(name="Canvas_Agent")))
+
+    c += 2
 
     for i in range(agents_per_process):
         port = int(argv[1]) + c
         agent_name = f'Ant_{i}'
 
-        ant = Ant(AID(name=agent_name), grid, food_position)
+        ant = Ant(AID(name=agent_name), current_pos=[0,0])
 
         agents.append(ant)
-        c += 1000
+        c += 2
 
     start_loop(agents)
 
