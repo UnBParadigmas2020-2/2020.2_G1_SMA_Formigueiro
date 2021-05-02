30842bdb833b30998f0793138937a1d5fe5fe287
Daniel Marques <danielmarques7@hotmail.com>
2021-05-02 14:05:58 -0300

    Create EnvironmentAgent class
    Co-authored-by: Lorrany Azevedo <lorrany90@gmail.com>

--- src/agents/Environment.py
+++ src/agents/Environment.py
@@ -10,3 +10,21 @@ class ComportTemporal(TimedBehaviour):
 
     def on_time(self):
         super(ComportTemporal, self).on_time()
+        self.agent.decrease()
+
+
+class EnvironmentAgent(Agent):
+    def __init__(self, aid, dimension=(30, 30)):
+        super(EnvironmentAgent, self).__init__(aid=aid)
+        x = range(0, dimension[0])
+        y = range(0, dimension[1])
+        self.food_position = [choice(x), choice(y)]
+        self.anthill_position = [choice(x), choice(y)]
+        self.behaviours.append(ComportTemporal(self, 1.))
+
+    def decrease(self):
+        temp_to_food = Grids().grid_to_food
+        for i in range(len(temp_to_food)):
+            for j in range(len(temp_to_food[0])):
+                if temp_to_food[i][j] >= 1:
+                    temp_to_food[i][j] -= 1
