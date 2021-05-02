ebcd0372b62e91de694f09272d8b0291947ad30f
Elias Bernardo <ebmm01@gmail.com>
2021-05-02 10:50:11 -0300

    Updating Ant path_find
    Co-authored-by: Fernando Miranda <ilusdasilva@gmail.com>

--- src/agents/Ant.py
+++ src/agents/Ant.py
@@ -3,6 +3,7 @@ from pade.core.agent import Agent
 from pade.misc.utility import display_message
 from ..grid.grid import Grids
 from ..grid.canvas import Canvas
+from ..settings import AGENT_TIME
 from pade.behaviours.protocols import TimedBehaviour
 
 
@@ -13,7 +14,11 @@ class ComportTemporal(TimedBehaviour):
     def on_time(self):
         super(ComportTemporal, self).on_time()
         self.agent.choose_path()
-        display_message(self.agent.aid.localname, f"Atual:: {self.agent.current_pos} Anterior:: {self.agent.last_pos}")
+
+        display_message(
+            self.agent.aid.localname, 
+            f"[{'Carregando' if self.agent.carrying else 'Procurando'}] Atual:: {self.agent.current_pos} Anterior:: {self.agent.last_pos}"
+        )
 
 class Ant(Agent):
     def __init__(self, aid, current_pos):
@@ -22,7 +27,7 @@ class Ant(Agent):
         self.current_pos = current_pos
         self.last_pos = current_pos
 
-        comp_temp = ComportTemporal(self, .2)
+        comp_temp = ComportTemporal(self, AGENT_TIME)
 
         self.behaviours.append(comp_temp)
 
@@ -33,87 +38,95 @@ class Ant(Agent):
         find_home = None
 
         if self.carrying:
-            for pos_x in range(3):
-                for pos_y in range(3):
-                    if [(self.current_pos[0] -1) + pos_x, (self.current_pos[1] -1) + pos_y] == Grids().initial_position:
-                        find_home = [(self.current_pos[0] - 1) + pos_x, (self.current_pos[1] -1) + pos_y]
-                        display_message(self.aid.localname, "Achei o formigueiro")
-                        break
-
-                    if (self.check_grid_limits(pos_x, pos_y)):
-                        continue
-
-                    to_home_path.append({ 
-                        "coordinates": [(self.current_pos[0] -1) + pos_x, (self.current_pos[1] -1) + pos_y], 
-                        "value": Grids().grid_to_home.item((self.current_pos[0] -1) + pos_x, (self.current_pos[1] -1) + pos_y)
-                    })
-
-            temp_to_food = Grids().grid_to_food
-            temp_to_food[self.current_pos[0], self.current_pos[1]] += 1
-            Grids().grid_to_food = temp_to_food
-
-            self.last_pos = self.current_pos
-
-
-            if find_home:
-                self.carrying = False
-                self.current_pos = find_home
- 
-            else:
-                to_home_path.sort(key=lambda lst: lst['value'], reverse=True)
-                self.current_pos = to_home_path[0]["coordinates"]
+            self.find_path_when_carring_food()
 
         # Procurando comida
         else:
-            for pos_x in range(3):
-                for pos_y in range(3):
-                    if [(self.current_pos[0] -1) + pos_x, (self.current_pos[1] -1) + pos_y] == Grids().food_position:
-                        find_food = [(self.current_pos[0] -1) + pos_x, (self.current_pos[1] -1) + pos_y]
-                        display_message(self.aid.localname, "Achei comida nos arredores")
-                        break
+            self.find_path_to_food()
 
-                    if (self.check_grid_limits(pos_x, pos_y)):
-                        continue
-
-                    elif food_path := Grids().grid_to_food.item((self.current_pos[0] -1) + pos_x, (self.current_pos[1] -1) + pos_y):                        
-                        to_food_path.append({ 
-                            "coordinates": [(self.current_pos[0] -1) + pos_x, (self.current_pos[1] -1) + pos_y], 
-                            "value": food_path
-                        })
-
-                    to_home_path.append({ 
-                        "coordinates": [(self.current_pos[0] -1) + pos_x, (self.current_pos[1] -1) + pos_y], 
-                        "value": Grids().grid_to_home.item((self.current_pos[0] -1) + pos_x, (self.current_pos[1] -1) + pos_y)
-                    })
-
-
-            #display_message(self.aid.localname, f"Home path possui {len(to_food_path)} elementos : {(to_food_path)} last position: {self.last_pos}")
-
-            temp_to_home = Grids().grid_to_home
-            temp_to_home[self.last_pos[0], self.last_pos[1]] += 1
-            Grids().grid_to_home = temp_to_home
-
-            self.last_pos = self.current_pos
-
-            if find_food:
-                self.carrying = True
-                self.current_pos = find_food
-
-            elif len(to_food_path) > 0:
-                to_food_path.sort(key=lambda lst: lst['value'], reverse=True)
-                self.current_pos = to_food_path[0]["coordinates"]
-
-            else:
-                to_home_path.sort(key=lambda lst: lst['value'])
-                no_pheromones = list(filter(lambda lst: lst['value'] == 0, to_home_path))
-
-                if len(no_pheromones) > 0:
-                    self.current_pos = choice(no_pheromones)["coordinates"]
-                else:
-                    self.current_pos = to_home_path[0]["coordinates"]
-            
         Canvas().update_ant_position(self.last_pos, self.current_pos, Canvas().RED)
 
+    def find_path_when_carring_food(self):
+        to_home_path = []
+        find_home = None
+
+        for pos_x in range(3):
+            for pos_y in range(3):
+                if [(self.current_pos[0] -1) + pos_x, (self.current_pos[1] -1) + pos_y] == Grids().initial_position:
+                    find_home = [(self.current_pos[0] - 1) + pos_x, (self.current_pos[1] -1) + pos_y]
+                    break
+
+                if (self.check_grid_limits(pos_x, pos_y)):
+                    continue
+
+                to_home_path.append({ 
+                    "coordinates": [(self.current_pos[0] -1) + pos_x, (self.current_pos[1] -1) + pos_y], 
+                    "value": Grids().grid_to_home.item((self.current_pos[0] -1) + pos_x, (self.current_pos[1] -1) + pos_y)
+                })
+
+        temp_to_food = Grids().grid_to_food
+        temp_to_food[self.last_pos[0], self.last_pos[1]] += 1
+        Grids().grid_to_food = temp_to_food
+
+        self.last_pos = self.current_pos
+
+        if find_home:
+            self.carrying = False
+            self.current_pos = find_home
+        else:
+            to_home_path.sort(key=lambda lst: lst['value'], reverse=True)
+            self.current_pos = to_home_path[0]["coordinates"]
+    
+    def find_path_to_food(self):
+        to_food_path = []
+        to_home_path = []
+        find_food = None
+
+        for pos_x in range(3):
+            for pos_y in range(3):
+                if [(self.current_pos[0] -1) + pos_x, (self.current_pos[1] -1) + pos_y] in Grids().food_positions:
+                    find_food = [(self.current_pos[0] -1) + pos_x, (self.current_pos[1] -1) + pos_y]
+                    break
+
+                if (self.check_grid_limits(pos_x, pos_y)):
+                    continue
+
+                elif food_path := Grids().grid_to_food.item((self.current_pos[0] -1) + pos_x, (self.current_pos[1] -1) + pos_y):                        
+                    to_food_path.append({ 
+                        "coordinates": [(self.current_pos[0] -1) + pos_x, (self.current_pos[1] -1) + pos_y], 
+                        "value": food_path
+                    })
+
+                to_home_path.append({ 
+                    "coordinates": [(self.current_pos[0] -1) + pos_x, (self.current_pos[1] -1) + pos_y], 
+                    "value": Grids().grid_to_home.item((self.current_pos[0] -1) + pos_x, (self.current_pos[1] -1) + pos_y)
+                })
+
+
+        temp_to_home = Grids().grid_to_home
+        temp_to_home[self.last_pos[0], self.last_pos[1]] += 1
+        Grids().grid_to_home = temp_to_home
+
+        self.last_pos = self.current_pos
+
+        if find_food:
+            self.carrying = True
+            self.current_pos = find_food
+
+        elif len(to_food_path) > 0:
+            to_food_path.sort(key=lambda lst: lst['value'], reverse=True)
+            self.current_pos = to_food_path[0]["coordinates"]
+
+        else:
+            to_home_path.sort(key=lambda lst: lst['value'])
+            no_pheromones = list(filter(lambda lst: lst['value'] == 0, to_home_path))
+
+            if len(no_pheromones) > 0:
+                self.current_pos = choice(no_pheromones)["coordinates"]
+
+            else:
+                self.current_pos = to_home_path[0]["coordinates"]
+    
     def check_grid_limits(self, pos_x, pos_y):
         if (self.current_pos[0] -1) + pos_x == self.last_pos[0] and \
             (self.current_pos[1] -1) + pos_y == self.last_pos[1]:
